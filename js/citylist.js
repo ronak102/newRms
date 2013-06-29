1733b3a10cbd3859d06765ebae55745654bb408f
Ronak <Koradiya>
2013-06-30 02:39:55 +0530

    11

--- js/citylist.js
+++ js/citylist.js
@@ -6,11 +6,7 @@ var citys;
 function onload1()
 {    
     alert("onload");
-    $.mobile.allowCrossDomainPages = true;
-        $.mobile.ajaxEnabled = true;
-        $.mobile.pushStateEnabled = false;
-        $.mobile.allowCrossDomainPages = true;
-    $.support.cors = true;
+    
     getCityList();
 }
 
@@ -24,74 +20,74 @@ function onload1()
 //alert(serviceURL + 'restaurants');
 function getCityList() {
     //    alert("call getCityList");
-$.ajax({
-           type: "GET",
-           url: serviceURL + 'city'
-        }).done(function( data ) {
-           alert(data);
-           citys = data;
-            $.each(citys, function(index, city) {                    
-                $('#cityList').append('<li  cityName="'+city.city+'" cityId="'+city.id+'"><a href="#">' + city.city +'</li>');            
-            });
-
-            $('#cityList li').click(function(){
-                //alert($(this).attr("cityId"));
-                $('#mypanel').panel("close");
-                $('#cityName').html($(this).attr("cityName"));//to replace other html containt
-                $("#otherLocation").attr("href","area.html?id="+$(this).attr("cityId"));
-            
-            });
-            $('#cityList').listview('refresh');
-        
-           
+    //$.ajax({
+    //           type: "GET",
+    //           url: serviceURL + 'city'
+    //        }).done(function( data ) {
+    //           alert(data);
+    //           citys = eval ("(" + data + ")");
+    //            $.each(citys, function(index, city) {                    
+    //                $('#cityList').append('<li  cityName="'+city.city+'" cityId="'+city.id+'"><a href="#">' + city.city +'</li>');            
+    //            });
+    //
+    //            $('#cityList li').click(function(){
+    //                //alert($(this).attr("cityId"));
+    //                $('#mypanel').panel("close");
+    //                $('#cityName').html($(this).attr("cityName"));//to replace other html containt
+    //                $("#otherLocation").attr("href","area.html?id="+$(this).attr("cityId"));
+    //            
+    //            });
+    //            $('#cityList').listview('refresh');
+    //        
+    //           
+    //        });
+    //    $.ajax({
+    //        url: serviceURL + 'city',                
+    //        crossDomain: true,
+    //        type: 'GET',
+    //        dataType: 'json',
+    //        error: function(x, textStatus, errorThrown){
+    ////            console.log( JSON.stringify(x));     
+    //            alert(JSON.stringify(x) + "  " + textStatus + "  " + errorThrown);     
+    //        },
+    //        success: function(data){
+    //            //             debug("data::"+JSON.stringify(data),2);
+    //                    
+    //            alert(data.length);
+    //            citys = data;
+    //            $.each(citys, function(index, city) {                    
+    //                $('#cityList').append('<li  cityName="'+city.city+'" cityId="'+city.id+'"><a href="#">' + city.city +'</li>');            
+    //            });
+    //
+    //            $('#cityList li').click(function(){
+    //                //alert($(this).attr("cityId"));
+    //                $('#mypanel').panel("close");
+    //                $('#cityName').html($(this).attr("cityName"));//to replace other html containt
+    //                $("#otherLocation").attr("href","area.html?id="+$(this).attr("cityId"));
+    //            
+    //            });
+    //            $('#cityList').listview('refresh');
+    //        }
+    //    });       
+    $.getJSON(serviceURL + 'city', function(data) {
+        $('#cityList li').remove();
+        citys = data;
+        //        alert(citys.length);
+                  
+        $.each(citys, function(index, city) {                    
+            $('#cityList').append('<li  cityName="'+city.city+'" cityId="'+city.id+'"><a href="#">' + city.city +'</li>');            
         });
-//    $.ajax({
-//        url: serviceURL + 'city',                
-//        crossDomain: true,
-//        type: 'GET',
-//        dataType: 'json',
-//        error: function(x, textStatus, errorThrown){
-////            console.log( JSON.stringify(x));     
-//            alert(JSON.stringify(x) + "  " + textStatus + "  " + errorThrown);     
-//        },
-//        success: function(data){
-//            //             debug("data::"+JSON.stringify(data),2);
-//                    
-//            alert(data.length);
-//            citys = data;
-//            $.each(citys, function(index, city) {                    
-//                $('#cityList').append('<li  cityName="'+city.city+'" cityId="'+city.id+'"><a href="#">' + city.city +'</li>');            
-//            });
-//
-//            $('#cityList li').click(function(){
-//                //alert($(this).attr("cityId"));
-//                $('#mypanel').panel("close");
-//                $('#cityName').html($(this).attr("cityName"));//to replace other html containt
-//                $("#otherLocation").attr("href","area.html?id="+$(this).attr("cityId"));
-//            
-//            });
-//            $('#cityList').listview('refresh');
-//        }
-//    });       
-//    $.get(serviceURL + 'city', function(data) {
-//        $('#cityList li').remove();
-//        citys = eval('(' + data + ')');
-//        //        alert(citys.length);
-//                  
-//        $.each(citys, function(index, city) {                    
-//            $('#cityList').append('<li  cityName="'+city.city+'" cityId="'+city.id+'"><a href="#">' + city.city +'</li>');            
-//        });
-//
-//        $('#cityList li').click(function(){
-//            //alert($(this).attr("cityId"));
-//            $('#mypanel').panel("close");
-//            $('#cityName').html($(this).attr("cityName"));//to replace other html containt
-//            $("#otherLocation").attr("href","area.html?id="+$(this).attr("cityId"));
-//            
-//        });
-//        
-//        $('#cityList').listview('refresh');
-//    });
+
+        $('#cityList li').click(function(){
+            //alert($(this).attr("cityId"));
+            $('#mypanel').panel("close");
+            $('#cityName').html($(this).attr("cityName"));//to replace other html containt
+            $("#otherLocation").attr("href","area.html?id="+$(this).attr("cityId"));
+            
+        });
+        
+        $('#cityList').listview('refresh');
+    });
 }
 
 //$('#cityList li').live('click', function() {
